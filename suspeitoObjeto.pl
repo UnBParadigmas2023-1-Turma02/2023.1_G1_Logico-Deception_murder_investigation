a18c87112b12cc73d6d9b62732610d5ac5ec9644
Pedro Haick <180129147@aluno.unb.br>
2023-05-30 11:43:12 -0300

    Relacionando objetos e vestigios com suspeitos

--- suspeitoObjeto.pl
+++ suspeitoObjeto.pl
@@ -1,1 +1,29 @@
-suspeitoTemObjeto()
+suspeitoObjeto(maria, injecao).
+suspeitoObjeto(maria, corda).
+suspeitoObjeto(maria, guarda-chuva).
+suspeitoObjeto(maria, explosivo).
+suspeitoObjeto(maria, motoserra).
+
+suspeitoObjeto(joao, machado).
+suspeitoObjeto(joao, veneno).
+suspeitoObjeto(joao, sniper).
+suspeitoObjeto(joao, picareta).
+suspeitoObjeto(joao, vinho).
+
+suspeitoObjeto(pedro, chute).
+suspeitoObjeto(pedro, cinto).
+suspeitoObjeto(pedro, martelo).
+suspeitoObjeto(pedro, chave).
+suspeitoObjeto(pedro, trofeu).
+
+suspeitoObjeto(ana, gasolina).
+suspeitoObjeto(ana, castical).
+suspeitoObjeto(ana, travesseiro).
+suspeitoObjeto(ana, tijolo).
+suspeitoObjeto(ana, cobra).
+
+suspeitoObjeto(carlos, faca).
+suspeitoObjeto(carlos, tesoura).
+suspeitoObjeto(carlos, fumaca).
+suspeitoObjeto(carlos, zarabatana).
+suspeitoObjeto(carlos, toalha).
--- suspeitoVestigio.pl
+++ suspeitoVestigio.pl
@@ -1,1 +1,29 @@
-suspeitoTemVestigio()
+suspeitoVestigio(maria, diamante).
+suspeitoVestigio(maria, caneca).
+suspeitoVestigio(maria, dinheiro).
+suspeitoVestigio(maria, computador).
+suspeitoVestigio(maria, cabelo).
+
+suspeitoVestigio(joao, gelo).
+suspeitoVestigio(joao, batom).
+suspeitoVestigio(joao, agulha).
+suspeitoVestigio(joao, cafe).
+suspeitoVestigio(joao, baralho).
+
+suspeitoVestigio(pedro, envelope).
+suspeitoVestigio(pedro, cueca).
+suspeitoVestigio(pedro, tinta).
+suspeitoVestigio(pedro, inseto).
+suspeitoVestigio(pedro, lanterna).
+
+suspeitoVestigio(ana, espelho).
+suspeitoVestigio(ana, cigarro).
+suspeitoVestigio(ana, violino).
+suspeitoVestigio(ana, colar).
+suspeitoVestigio(ana, rato).
+
+suspeitoVestigio(carlos, sangue).
+suspeitoVestigio(carlos, tinta).
+suspeitoVestigio(carlos, luvas).
+suspeitoVestigio(carlos, ovos).
+suspeitoVestigio(carlos, mapa).
